954276cb50910b81b603c64e49feb45d86d5f90e
rfinnz <finneyrm@gmail.com>
2019-01-30 17:50:15 -0500

    plz commit

--- PA4.m
+++ PA4.m
@@ -12,13 +12,26 @@ V(:,10) = 0;
 maxit = 100;
 
 
-
-for j=2:1:ny-1
+for i =1:maxit
+for j=1:ny
 for i=2:1:nx-1
     
-V(i,j) = V(i+1,j) - 2*V(i, j) + V(i-1,j) + V(i,j+1) - 2*V(i, j) + V(i,j-1);
+    if (j ==1)
+        V(i,j) = (V(i+1,j) + V(i-1,j) + V(i,j+1) - 2*V(i, j))/3;
+   
+    
+    elseif (j==ny)
+        V(i,j) = (V(i+1,j)+ V(i-1,j) + V(i,j-1))/3;
+        
+    else
+       
+V(i,j) = (V(i+1,j) + V(i-1,j) + V(i,j+1) + V(i,j-1))/4;
 pause(0.5)
-surf(V,'EdgeColor','none');
+surf(V);
 
+V(:,1) = 1;
+V(:,10) = 0;
+end
+end
 end
 end
